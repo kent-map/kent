f27cdbcec2ab08eb77243b913ce6cb0758421a6f
Ron Snyder <ron@snyderjr.com>
2023-08-31 13:29:01 -0400

    Updated dev run script.

--- webapp/run.sh
+++ webapp/run.sh
@@ -2,4 +2,4 @@
 
 cd "$(dirname "$0")"
 source .venv/bin/activate
-./main.py --content .. --api http://localhost:8000 --port 9000 --wc http://localhost:5173/src/main.ts
+./main.py --content .. --api http://localhost:8080 --wc http://localhost:5173/src/main.ts --port 9000 
