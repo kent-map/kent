973e036ae33d3de064b1abbe5f6245b966215c26
Ron Snyder <ron@snyderjr.com>
2023-10-21 18:28:10 -0400

    testing

--- juncture/index.js
+++ juncture/index.js
@@ -23,10 +23,11 @@ function createJunctureV1App() {
       let path = location.pathname.split('/').slice(1)
       let numPops = relPath.filter(pe => pe === '..').length
       bannerUrl = `/${path.slice(0,-numPops).join('/')}/${relPath.slice(numPops).join('/')}`
-      el.setAttribute('banner', bannerUrl)
+      console.log(path, relPath, numPops, bannerUrl)
+      // el.setAttribute('banner', bannerUrl)
     }
   })
-
+  
   let main = document.querySelector('main')
   let tmp = new DOMParser().parseFromString(main.innerHTML, 'text/html').children[0].children[1]
 
