4880eb98a0a1fc8fbca5e62b793f8cb17e8246d6
Ron Snyder <ron@snyderjr.com>
2023-10-23 12:37:43 -0400

    Site search fix

--- juncture/src/components/SiteSearch.ce.vue
+++ juncture/src/components/SiteSearch.ce.vue
@@ -1,12 +1,9 @@
 <template>
-  <div ref="root" class="search">
+  <div ref="root" class="search" @click="open">
     <input class="search-btn" type="checkbox" id="search-btn"/>
     <div class="wrapper">
       <div class="search-form">
-        <div class="input-wrapper">
-          <span @click="cancel">
-            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg>
-          </span>
+        <div class="input-wrapper relative">
           <input
             type="text"
             id="search-input"
@@ -17,6 +14,7 @@
             placeholder="Search site"
           />
         </div>      
+        <svg @click="cancel" class="absolute top-[12px] right-[10px] z-10 cursor-pointer" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512" fill="#000"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg>
       </div>
       <ul v-if="searchResults.length > 0" class="search-results">
         <li v-for="item, idx in searchResults" :key="`sr=${idx}`">
@@ -32,7 +30,7 @@
     </div>
     <label class="search-icon" htmlFor="search-btn">
       <span @click="searchClicked">
-        <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512" fill="#fff"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/></svg>
       </span>
     </label>
   </div>
@@ -63,8 +61,12 @@
   const totalResults = ref(0)
   const isVisible = ref(false)
 
-
   onMounted(() => init())
+
+  function open(evt:MouseEvent) {
+    evt.preventDefault()
+    isVisible.value = true
+  }
 
   function init() {
     searchInput.value = shadowRoot.value?.querySelector('#search-input') as HTMLInputElement
@@ -106,7 +108,9 @@
     if (evt.key === 'Enter') doSearch()
   }
   
-  function cancel() {
+  function cancel(evt:MouseEvent) {
+    evt.preventDefault()
+    evt.stopPropagation()
     isVisible.value = false
     if (searchInput.value) searchInput.value.value = ''
     currentQuery.value = ''
@@ -148,6 +152,8 @@
 </script>
 
 <style>
+
+  @import '../tailwind.css';
 
   * { box-sizing: border-box; } 
 
@@ -266,6 +272,7 @@
     position: relative
   }
 
+  /*
   .cancel {
     position: absolute;
     right: 0;
@@ -273,5 +280,6 @@
     font-size: 16px;
     cursor: pointer;
   }
+  */
 
 </style>
